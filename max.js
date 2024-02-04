efa86fd1feae0f08f5943e63c881717d1103b8ff
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-04 21:17:42 +0600

    max related problem

---
+++ max.js
@@ -0,0 +1,18 @@
+const disha = 56;
+const salman = 95;
+
+
+if(disha > salman) {
+    console.log('Disha will get the strawberry');
+}
+else {
+    console.log('Salman will get the strawberry');
+}
+
+
+// function
+const myMax = function(a, b) {
+    return a > b ? a : b;
+}
+
+console.log(myMax(3, 5));
